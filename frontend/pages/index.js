40df92b44bb40ce45090927160cb4bc9c53b5882
unknown <cindy.surjawan@banksinarmas.com>
2022-09-28 16:30:53 +0700

    add text by

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -65,6 +65,8 @@ function Input({onSuccess}) {
   return (
     <div>
       <h1 className='w-full text-3xl font-bold text-red-700 uppercase md:text-4xl'>Bank Sinarmas - PUSILKOM</h1>
+      <h3 className='w-full text-3xl font-bold text-red-700 uppercase md:text-4xl'>By Cindy Surjawan</h3>
+    
       {error && <p className='text-red-600'>error: {error.toString()}</p>}
       {data && <p className='text-green-500'>success: {data}</p>}
       <form className='p-4' onSubmit={handleSubmit}>
