3311f53aa0c8dd0da7a6cd07781378b1ed98d130
unknown <cindy.surjawan@banksinarmas.com>
2022-09-29 14:47:01 +0700

    add testing, mvc, and modif frontend

---
+++ backend/api/routes.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	"github.com/cindysurjawann/helloworld/todos"
+	"github.com/gin-contrib/cors"
+)
+
+func (s *server) SetupRouter() {
+	s.Router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+	}))
+
+	todosRepo := todos.NewRepository(s.DB)
+	todosService := todos.NewService(todosRepo)
+	todosHandler := todos.NewHandler(todosService)
+
+	s.Router.GET("/", todosHandler.GetTodos)
+	s.Router.POST("/send", todosHandler.CreateTodo)
+}
